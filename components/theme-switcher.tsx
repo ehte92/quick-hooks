9eebf4540fb0e63d1816bcdafa73d0083a832657
Ehtesham Ansari <ehte92@gmail.com>
2024-07-02 12:40:04 +0400

    chore: Refactor ThemeSwitcher component and add Button component

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -5,19 +5,22 @@ import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
+import { Button } from '@/components/ui/button';
+
 export function ThemeSwitcher() {
   const { setTheme, theme } = useTheme();
 
   return (
     <>
-      <button
-        className="border-border dark:border-darkBorder dark:bg-darkBg mt-6 h-11 w-11 border-2 bg-white p-0"
+      <Button
+        size="icon"
+        className="fixed right-10 top-10 z-50"
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       >
-        <Sun className="stroke-darkText hidden h-6 w-6 dark:inline" />
-        <Moon className="stroke-text inline h-6 w-6 dark:hidden" />
+        <Sun className="stroke-text hidden h-6 w-6 w500:h-4 w500:w-4 dark:inline" />
+        <Moon className="stroke-text inline h-6 w-6 w500:h-4 w500:w-4 dark:hidden" />
         <span className="sr-only">Toggle theme</span>
-      </button>
+      </Button>
     </>
   );
 }
