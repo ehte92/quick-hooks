f5308800659f8905dd40dc1b3e454025b2ab2705
Ehtesham Ansari <ehte92@gmail.com>
2024-07-04 15:27:40 +0400

    refactor: Update BackButton component to use Button component for improved reusability and consistency

--- components/ui/back-button.tsx
+++ components/ui/back-button.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { ArrowLeftIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
+import { Button } from './button';
+
 const BackButton = () => {
   const router = useRouter();
 
@@ -11,12 +13,15 @@ const BackButton = () => {
   };
 
   return (
-    <div
-      className="bg-main cursor-pointer p-2 border-2 border-border dark:border-darkBorder shadow-light dark:shadow-dark hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none dark:hover:shadow-none"
-      onClick={handleBack}
-    >
+    // <div
+    //   className="bg-main cursor-pointer rounded-base p-2 border-2 border-border dark:border-darkBorder shadow-light dark:shadow-dark hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none dark:hover:shadow-none"
+    //   onClick={handleBack}
+    // >
+    //   <ArrowLeftIcon className="w-6 h-6" />
+    // </div>
+    <Button size="icon" onClick={handleBack}>
       <ArrowLeftIcon className="w-6 h-6" />
-    </div>
+    </Button>
   );
 };
 
