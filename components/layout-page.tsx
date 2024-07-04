2baf78c760cd6a9718be4f46ae868b1ab5bcdf32
Ehtesham Ansari <ehte92@gmail.com>
2024-07-04 15:27:50 +0400

    feat: Create LayoutPage component for consistent page layout and styling

---
+++ components/layout-page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { title } from 'process';
+
+import BackButton from './ui/back-button';
+
+interface LayoutPageProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const LayoutPage = ({ title, children }: LayoutPageProps) => {
+  return (
+    <div className="container mx-auto rounded-base bg-bg dark:bg-darkBg p-4 font-mono text-text dark:text-darkText border shadow-light dark:shadow-dark my-4">
+      <div className="flex items-center justify-between">
+        <BackButton />
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <div></div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default LayoutPage;
